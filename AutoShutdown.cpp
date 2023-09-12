6fef2132a53314551be41b27125f23304e0a4f1a
FredrickHZO <federico.armagno97@gmail.com>
2023-09-12 19:05:46 +0200

    Application entry point.

---
+++ AutoShutdown.cpp
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#include "Window.h"
+#include "Time.h"
+
+int WINAPI wWinMain(HINSTANCE hInst,
+        HINSTANCE hPrevInst,
+        PWSTR pCmdLine,
+        int ncmdshow) {
+
+    Window* window = new Window(hInst);
+
+    ShowWindow(window->getHandle(), ncmdshow);
+    window->ProcessMessages();
+
+    delete window;
+    return 0;
+ }
